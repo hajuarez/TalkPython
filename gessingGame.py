aa1c9a93a1ca060be58210eecc5c4c5f139b26a1
hajuarez <hajuarez@prueba.com>
2026-05-02 12:40:38 -0600

    Add M&M guessing game script

---
+++ gessingGame.py
@@ -0,0 +1,12 @@
+import random
+
+print("--------------------------------------------")
+print("     M&M guessing game!")
+print("--------------------------------------------")
+
+mm_count = random.randint(1, 100)
+
+print("Guess the number of M&Ms and you get lunch on the house!")
+print()
+
+
