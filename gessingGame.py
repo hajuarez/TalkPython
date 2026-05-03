767cc68a5af41ce91219d5931b0b4411d6bad9d7
hajuarez <hajuarez@prueba.com>
2026-05-03 16:09:49 -0600

    Adding check messages to guide the user(correct number/too Low/too High)

--- gessingGame.py
+++ gessingGame.py
@@ -4,9 +4,25 @@ print("--------------------------------------------")
 print("     M&M guessing game!")
 print("--------------------------------------------")
 
-mm_count = random.randint(1, 100)
-
 print("Guess the number of M&Ms and you get lunch on the house!")
 print()
 
+mm_count = 79#random.randint(1, 100)
+attempt_limit = 8
+attempts = 0
 
+while attempts < attempt_limit:
+    guess = int(input("How many M&Ms are in the jar? "))
+    attempts += 1
+
+    if guess == mm_count:
+        print("Congratulations! You guessed the correct number of M&Ms!")
+        break
+    elif guess < mm_count:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+    
+
+print("Number of M&Ms in the jar: ", mm_count)
+print(f"Bye, you had {attempts} attempts!")    
