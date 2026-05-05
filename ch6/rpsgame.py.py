8d7f679a02e9dd783a721b94691c02df155e847c
hajuarez <hajuarez@prueba.com>
2026-05-04 19:46:25 -0600

    enhancement: cleaning up this program inserting functions

--- ch6/rpsgame.py.py
+++ ch6/rpsgame.py.py
@@ -1,58 +1,64 @@
 import random
 
-print("--------------------------------")
-print("Rock Paper Scissors v1")
-print("--------------------------------")
+def main():
+    show_header()
 
-player_1 = "You"
-player_2 = "Computer"
+    player = "You"
+    ai = "Computer"
 
-rolls = ["rock", "paper", "scissors"]
+    play_game(player, ai)
 
-roll1 = input(f"{player_1}, enter your roll [rock, paper, scissors]: ")
-roll1 = roll1.lower().strip() # Convert to lowercase to make it case-insensitive
-if roll1 not in rolls:
-    print(f"Sorry {player_1}, {roll1} is not a valid play!")
+def show_header():
+    print("--------------------------------")
+    print("Rock Paper Scissors v1")
+    print("--------------------------------")
 
-#2nd player automated, no need to validate input
-roll2 = random.choice(rolls)
+def play_game(player_1, player_2):
+    rolls = ["rock", "paper", "scissors"]
 
-print(f"{player_1} rolls {roll1}")
-print(f"{player_2} rolls {roll2}")
+    roll1 = input(f"{player_1}, enter your roll [rock, paper, scissors]: ")
+    roll1 = roll1.lower().strip() # Convert to lowercase to make it case-insensitive
+    if roll1 not in rolls:
+        print(f"Sorry {player_1}, {roll1} is not a valid play!")
 
-#Test for a winner
-# Rock
-#   Rock -> tie
-#   Paper -> lose
-#   Scissors -> win
-# Paper
-#   Rock -> win
-#   Paper -> tie
-#   Scissors -> lose
-# Scissors
-#   Rock -> lose
-#   Paper -> win
-#   Scissors -> tie
+    #2nd player automated, no need to validate input
+    roll2 = random.choice(rolls)
 
-winner = None
+    print(f"{player_1} rolls {roll1}")
+    print(f"{player_2} rolls {roll2}")
 
-if roll1 == roll2:
-    print("It's a tie!")
-elif roll1 == "rock":
-    if roll2 == "scissors":
-        print(f"{player_1} wins!")
-    else:
-        print(f"{player_2} wins!")
-elif roll1 == "paper":
-    if roll2 == "rock":
-        print(f"{player_1} wins!")
-    else:
-        print(f"{player_2} wins!")
-elif roll1 == "scissors":
-    if roll2 == "paper":
-        print(f"{player_1} wins!")
-    else:
-        print(f"{player_2} wins!")
+    #Test for a winner
+    # Rock
+    #   Rock -> tie
+    #   Paper -> lose
+    #   Scissors -> win
+    # Paper
+    #   Rock -> win
+    #   Paper -> tie
+    #   Scissors -> lose
+    # Scissors
+    #   Rock -> lose
+    #   Paper -> win
+    #   Scissors -> tie
 
+    winner = None
 
+    if roll1 == roll2:
+        print("It's a tie!")
+    elif roll1 == "rock":
+        if roll2 == "scissors":
+            print(f"{player_1} wins!")
+        else:
+            print(f"{player_2} wins!")
+    elif roll1 == "paper":
+        if roll2 == "rock":
+            print(f"{player_1} wins!")
+        else:
+            print(f"{player_2} wins!")
+    elif roll1 == "scissors":
+        if roll2 == "paper":
+            print(f"{player_1} wins!")
+        else:
+            print(f"{player_2} wins!")
 
+main()
