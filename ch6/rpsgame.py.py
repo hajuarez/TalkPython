9bb6e90ab18b2f7d876e72a3fcc7dd56a78cc695
hajuarez <hajuarez@prueba.com>
2026-05-04 21:06:22 -0600

    enahncement: split functionality of play_game, introducing determine_winner()

--- ch6/rpsgame.py.py
+++ ch6/rpsgame.py.py
@@ -2,11 +2,7 @@ import random
 
 def main():
     show_header()
-
-    player = "You"
-    ai = "Computer"
-
-    play_game(player, ai)
+    play_game("You", "Computer")
 
 def show_header():
     print("--------------------------------")
@@ -17,6 +13,7 @@ def play_game(player_1, player_2):
     rolls = ["rock", "paper", "scissors"]
 
     roll1 = get_roll(player_1, rolls)
+    #roll2 = get_roll(player_2, rolls)
     roll2 = random.choice(rolls)                #2nd player automated, no need to validate input
 
     if roll1 is None:
@@ -26,39 +23,25 @@ def play_game(player_1, player_2):
     print(f"{player_1} rolls {roll1}")
     print(f"{player_2} rolls {roll2}")
 
-    #Test for a winner
-    # Rock
-    #   Rock -> tie
-    #   Paper -> lose
-    #   Scissors -> win
-    # Paper
-    #   Rock -> win
-    #   Paper -> tie
-    #   Scissors -> lose
-    # Scissors
-    #   Rock -> lose
-    #   Paper -> win
-    #   Scissors -> tie
-
-    winner = None
-
-    if roll1 == roll2:
+    winner = determine_winner(roll1, roll2, player_1, player_2)
+    if winner is None:
         print("It's a tie!")
+    else:
+        print(f"{winner} wins!")
+
+def determine_winner(roll1, roll2, player1, player2):
+    # Rules:
+    # Rock beats Scissors
+    # Scissors beats Paper
+    # Paper beats Rock
+    if roll1 == roll2:
+        return None
     elif roll1 == "rock":
-        if roll2 == "scissors":
-            print(f"{player_1} wins!")
-        else:
-            print(f"{player_2} wins!")
+        return player1 if roll2 == "scissors" else player2
     elif roll1 == "paper":
-        if roll2 == "rock":
-            print(f"{player_1} wins!")
-        else:
-            print(f"{player_2} wins!")
+        return player1 if roll2 == "rock" else player2
     elif roll1 == "scissors":
-        if roll2 == "paper":
-            print(f"{player_1} wins!")
-        else:
-            print(f"{player_2} wins!")
+        return player1 if roll2 == "paper" else player2
 
 def get_roll(player_name, rolls):
     roll = input(f"{player_name}, enter your roll [rock, paper, scissors]: ")
